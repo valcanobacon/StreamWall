50f49704d27d810f4d054f821658f7cca35eebf3
Valcano Bacon <valcanobacon.github.vmcfd@simplelogin.co>
2022-01-27 13:10:30 -0800

    Implemented cost per ts file

--- main.go
+++ main.go
@@ -4,14 +4,62 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path"
+	"path/filepath"
+	"strings"
+	"os"
+	"bufio"
+	"strconv"
 
 	//"github.com/go-chi/chi"
 )
+
+var durations map[string]float64
 
 func main() {
 	// configure the songs directory name and port
 	const songsDir = "songs"
 	const port = 8080
+
+
+	durationFiles, err := filepath.Glob("songs/*/durations.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	durations = map[string]float64{}
+
+	for _, durationFilePath := range durationFiles {
+		durationFilePathWithoutPrefix := strings.Replace(durationFilePath, "songs/", "/", 1)
+		songDir, _ := path.Split(durationFilePathWithoutPrefix)
+		fmt.Println(songDir)
+
+
+		file, err := os.Open(durationFilePath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			row := scanner.Text()
+			columns := strings.Split(row, " ")
+			if duration, err := strconv.ParseFloat(columns[1], 64); err == nil {
+				durations[songDir + columns[0]] = duration
+			} else {
+				fmt.Println(err)
+			}
+
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(durations)
+
+	}
 
 	// add a handler for the song files
 	http.Handle("/", addHeaders(monitorTimeForCredits(http.FileServer(http.Dir(songsDir)))))
@@ -32,7 +80,10 @@ func addHeaders(h http.Handler) http.HandlerFunc {
 
 func monitorTimeForCredits(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%v", r.URL)
+		duration := durations[r.URL.String()]
+		satsPerSecond := 0.5
+		cost := duration * satsPerSecond
+		log.Printf("%v %g seconds at %g costs %g", r.URL, duration, satsPerSecond, cost)
 		h.ServeHTTP(w, r)
 	}
 }
